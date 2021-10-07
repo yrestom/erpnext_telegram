49d5cb0931daa031eead9ef8b201c9870b2d4001
Youssef <youssefrestom@gmail.com>
2021-10-07 12:14:05 +0000

    fix: send date notification to user email

--- erpnext_telegram_integration/extra_notifications/doctype/extra_notification_log/extra_notification_log.py
+++ erpnext_telegram_integration/extra_notifications/doctype/extra_notification_log/extra_notification_log.py
@@ -5,23 +5,32 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
-from frappe.desk.doctype.notification_log.notification_log import enqueue_create_notification
+from frappe.desk.doctype.notification_log.notification_log import (
+    enqueue_create_notification,
+)
+
 
 class ExtraNotificationLog(Document):
-	def after_insert(self):
-		self.make_notification_log()
+    def after_insert(self):
+        self.make_notification_log()
 
+    def make_notification_log(self):
+        alert_doc = frappe.get_doc(self.doctype_name, self.doc_name)
+        users = []
+        owner_email = frappe.get_value("User", alert_doc.owner, "email")
+        if owner_email:
+            users.append(owner_email)
+        modified_by_email = frappe.get_value("User", alert_doc.modified_by, "email")
+        if modified_by_email:
+            users.append(modified_by_email)
+        if len(users) == 0:
+            return
+        notification_doc = {
+            "type": "Share",
+            "document_type": self.doctype_name,
+            "subject": self.subject,
+            "document_name": self.doc_name,
+            "from_user": frappe.session.user,
+        }
 
-	def make_notification_log(self):
-			alert_doc = frappe.get_doc(self.doctype_name,self.doc_name)
-			users = [alert_doc.owner, alert_doc.modified_by]
-			notification_doc = {
-				'type': 'Share',
-				'document_type': self.doctype_name,
-				'subject': self.subject,
-				'document_name': self.doc_name,
-				'from_user': frappe.session.user
-			}
-			
-			enqueue_create_notification(users, notification_doc)
-	
+        enqueue_create_notification(users, notification_doc)
